11c5541103138a803ea4eeda1d68037498d64acb
Khoi.LN <147135167+khoilen@users.noreply.github.com>
2024-12-09 07:33:03 +0700

    feat: initalization css frame-work (#4)
    * feat: add css frame work and pushlish css frame work
    
    * feat: edit github action when publish
    
    * feat: add nx.json file

---
+++ apps/nt-css/themes/spacing.ts
@@ -0,0 +1,17 @@
+export const spacing = {
+    xxxlg: '5rem',
+    xxlg: '4rem',
+    xlg: '3rem',
+    lg: '2.25rem',
+    DEFAULT: '1.5rem',
+    sm: '1.125rem',
+    xs: '0.75rem',
+    xxs: '0.5rem',
+    xxxs: '0.25rem',
+}
+export const gap = {
+    lg: '1.125rem',
+    DEFAULT: '0.75rem',
+    sm: '0.5rem',
+    xs: '0.375rem',
+}
