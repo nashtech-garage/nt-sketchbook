76a514f77113da0479b30e9047fd19fced65ef90
khoinashtech <khoilen@nashtechglobal.com>
2024-12-10 16:38:40 +0700

    feat: update code for today

--- apps/nt-stylesheet/vite.config.js
+++ apps/nt-stylesheet/vite.config.js
@@ -2,13 +2,19 @@ import { defineConfig } from 'vite'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import viteSassDts from 'vite-plugin-sass-dts'
+import path from 'path'
 
 export default defineConfig({
     build: {
+        lib: {
+            entry: path.resolve(__dirname, 'themes/index.ts'),
+            name: 'ntStylesheetThemes',
+            fileName: (format) => `themes.${format}.js`,
+        },
         outDir: 'dist',
         cssCodeSplit: false,
         rollupOptions: {
-            input: './src/styles.scss',
+            input: path.resolve(__dirname, 'src/styles.scss'),
             output: {
                 entryFileNames: 'styles.css',
                 chunkFileNames: 'styles.css',
