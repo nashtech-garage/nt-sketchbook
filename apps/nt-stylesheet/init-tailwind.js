ac31db41948b43f44dda985b29776313405a54f2
khoinashtech <khoilen@nashtechglobal.com>
2024-12-13 16:06:50 +0700

    feat: detect will run enviorment

--- apps/nt-stylesheet/init-tailwind.js
+++ apps/nt-stylesheet/init-tailwind.js
@@ -2,64 +2,73 @@ import fs from 'fs'
 import { exec } from 'child_process'
 import readline from 'readline'
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-})
+const isNotDevEnvironment =
+    !process.env.INIT_CWD || process.env.INIT_CWD !== process.cwd()
 
-rl.question(
-    'Do you want to initialize Tailwind CSS? (y/n) ',
-    (answer) => {
-        if (answer.toLowerCase() === 'y') {
-            exec('npx tailwindcss init', (error, stdout, stderr) => {
-                if (error) {
-                    console.error(
-                        `Error initializing Tailwind CSS: ${error.message}`,
-                    )
-                    rl.close()
-                    return
-                }
-                if (stderr) {
-                    console.error(
-                        `Error initializing Tailwind CSS: ${stderr}`,
-                    )
-                    rl.close()
-                    return
-                }
+if (isNotDevEnvironment) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
 
-                const tailwindConfigContent = `
-                    /** @type {import('tailwindcss').Config} */
-                    const ntTheme = require("nt-stylesheet/dist/theme.cjs");
-
-                    module.exports = {
-                        content: ['*.{html,js}'],
-                        theme: {
-                            extend: ntTheme.theme.extend,
-                        },
-                        plugins: [],
-                    };
-                `
-                fs.writeFile(
-                    'tailwind.config.js',
-                    tailwindConfigContent,
-                    (writeError) => {
-                        if (writeError) {
-                            console.error(
-                                `Error writing tailwind.config.js: ${writeError.message}`,
-                            )
-                            rl.close()
-                            return
-                        }
-                        console.log(
-                            'tailwind.config.js has been initialized and customized successfully!',
+    rl.question(
+        'Do you want to initialize Tailwind CSS? (y/n) ',
+        (answer) => {
+            if (answer.toLowerCase() === 'y') {
+                exec('npx tailwindcss init', (error, _, stderr) => {
+                    if (error) {
+                        console.error(
+                            `Error initializing Tailwind CSS: ${error.message}`,
                         )
                         rl.close()
-                    },
-                )
-            })
-        } else {
-            console.log('Tailwind CSS initialization skipped.')
-            rl.close()
-        }
-    },
-)
+                        return
+                    }
+                    if (stderr) {
+                        console.error(
+                            `Error initializing Tailwind CSS: ${stderr}`,
+                        )
+                        rl.close()
+                        return
+                    }
+
+                    const tailwindConfigContent = `
+                        /** @type {import('tailwindcss').Config} */
+                            const ntTheme = require('nt-stylesheet/dist/theme.cjs');
+                            module.exports = {
+                                content: ['*.{html,js}'],
+                                theme: {
+                                    extend: ntTheme.theme.extend,
+                                },
+                                plugins: [],
+                            };
+                        `
+
+                    fs.writeFile(
+                        'tailwind.config.js',
+                        tailwindConfigContent,
+                        (writeError) => {
+                            if (writeError) {
+                                console.error(
+                                    `Error writing tailwind.config.js: ${writeError.message}`,
+                                )
+                                rl.close()
+                                return
+                            }
+                            console.log(
+                                'tailwind.config.js has been initialized and customized successfully!',
+                            )
+                            rl.close()
+                        },
+                    )
+                })
+            } else {
+                console.log('Tailwind CSS initialization skipped.')
+                rl.close()
+            }
+        },
+    )
+} else {
+    console.log(
+        'Development environment detected, skipping Tailwind CSS initialization.',
+    )
+}
