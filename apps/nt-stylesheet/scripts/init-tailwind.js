f984c4af13f552a46c5df7dbb55d63d09140b77c
khoinashtech <khoilen@nashtechglobal.com>
2024-12-17 15:27:41 +0700

    feat: move init-tailwind to folder scripts

--- apps/nt-stylesheet/scripts/init-tailwind.js
+++ apps/nt-stylesheet/scripts/init-tailwind.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
-import { exec } from 'child_process'
 import readline from 'readline'
-const isNotDevEnvironment =
-    !process.env.INIT_CWD || process.env.INIT_CWD !== process.cwd()
 
-const createTailwindConfig = (rl) => {
+// Check if running in an interactive environment
+const isInteractive = process.stdout.isTTY
+
+const createTailwindConfig = () => {
     const tailwindConfigContent = `/** @type {import('tailwindcss').Config} */
 const ntTheme = require('nt-stylesheet/dist/theme.cjs');
 module.exports = {
@@ -23,18 +23,16 @@ module.exports = {
                 console.error(
                     `Error writing tailwind.config.js: ${writeError.message}`,
                 )
-                rl.close()
                 return
             }
             console.log(
-                'tailwind.config.js has been initialized and customized successfully!',
+                'tailwind.config.js has been initialized successfully!',
             )
-            rl.close()
         },
     )
 }
 
-const createPostCSSConfig = (rl) => {
+const createPostCSSConfig = () => {
     const postcssConfigContent = `module.exports = {
     plugins: {
         'postcss-import': {},
@@ -53,18 +51,16 @@ const createPostCSSConfig = (rl) => {
                 console.error(
                     `Error writing postcss.config.js: ${writeError.message}`,
                 )
-                rl.close()
                 return
             }
             console.log(
                 'postcss.config.js has been created successfully!',
             )
-            rl.close()
         },
     )
 }
 
-if (isNotDevEnvironment) {
+if (isInteractive) {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -74,33 +70,14 @@ if (isNotDevEnvironment) {
         'Do you want to initialize Tailwind CSS? (y/n) ',
         (answer) => {
             if (answer.toLowerCase() === 'y') {
-                exec('npx tailwindcss init', (error, _, stderr) => {
-                    if (error) {
-                        console.error(
-                            `Error initializing Tailwind CSS: ${error.message}`,
-                        )
-                        rl.close()
-                        return
-                    }
-                    if (stderr) {
-                        console.error(
-                            `Error initializing Tailwind CSS: ${stderr}`,
-                        )
-                        rl.close()
-                        return
-                    }
-
-                    createTailwindConfig(rl)
-                    createPostCSSConfig(rl)
-                })
+                createTailwindConfig()
+                createPostCSSConfig()
             } else {
                 console.log('Tailwind CSS initialization skipped.')
-                rl.close()
             }
+            rl.close()
         },
     )
 } else {
-    console.log(
-        'Development environment detected, skipping Tailwind CSS initialization.',
-    )
+    console.log('Non-interactive mode detected.')
 }
