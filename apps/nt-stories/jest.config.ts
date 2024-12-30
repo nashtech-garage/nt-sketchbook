986bfc46558b9b5a94d985847dbf486d1fe474a0
Tin Thai <tin.thanh.thai@gmail.com>
2024-12-30 09:28:50 +0700

    build: init demo app
    Init demo app

---
+++ apps/nt-stories/jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+    displayName: 'nt-stories',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)':
+            '@nx/react/plugins/jest',
+        '^.+\\.[tj]sx?$': [
+            'babel-jest',
+            { presets: ['@nx/next/babel'] },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/apps/nt-stories',
+}
