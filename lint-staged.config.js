083aefc6233a6562d5c72a76104831e07fdbbc4f
khoilen <khoilen@nashtechglobal.com>
2025-01-04 07:38:29 +0700

    fix: smoke test precommit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,16 +1,8 @@
 module.exports = {
-    '{apps}/**/*.{ts,tsx}': (files) => {
-        return `node nx-affected-relative-paths.js --target=typecheck --files=${files.join(
-            ',',
-        )}`
-    },
     '{apps}/**/*.{js,ts,jsx,tsx,json,css,scss,md}': (files) => {
         return [
             `prettier --write ${files.join(' ')}`,
-            `node nx-affected-relative-paths.js --target=lint --files=${files.join(
-                ',',
-            )}`,
-            `nx format:write --files=${files.join(',')}`,
+            `pnpm eslint --fix --files=${files.join(',')}`,
         ]
     },
 }
