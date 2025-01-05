827df5037efd88d158cf20bdacc2bee0ac8d17d9
khoilen <khoilen@nashtechglobal.com>
2025-01-05 14:42:30 +0700

    feat(release): add release auto fetch version

--- release.config.cjs
+++ release.config.cjs
@@ -3,4 +3,28 @@
  */
 module.exports = {
     branches: ['main', { name: 'beta', prerelease: true }],
+    repositoryUrl: 'https://github.com/nashtech-garage/nt-sketchbook',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        [
+            '@semantic-release/npm',
+            {
+                pkgRoot: 'apps/nt-stylesheet',
+            },
+        ],
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: [
+                    'apps/nt-stylesheet/package.json',
+                    'apps/nt-stylesheet/CHANGELOG.md',
+                ],
+                message:
+                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
 }
