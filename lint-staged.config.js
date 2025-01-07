73b9f7edecf4c2e27433972976ccb4914d0c3550
khoilen <khoilen@nashtechglobal.com>
2025-01-07 10:00:47 +0700

    feat: update lint stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    '{apps}/**/*.{js,ts,jsx,tsx,json,css,scss,md}': (files) => {
+    '**/*.{js,ts,jsx,tsx,json,css,scss,md}': (files) => {
         return [
             `prettier --write ${files.join(' ')}`,
             `pnpm eslint . --fix`,
