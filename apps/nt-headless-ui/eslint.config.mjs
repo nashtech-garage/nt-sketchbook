2f6a3c991ea96039395c8e4746d62a45e2689378
khoilen <khoilen@nashtechglobal.com>
2025-01-22 09:17:28 +0700

    feat: update eslint

--- apps/nt-headless-ui/eslint.config.mjs
+++ apps/nt-headless-ui/eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    ...compat.extends('eslint:recommended', 'prettier'),
 ]
 
 export default eslintConfig
