f83e9fc4eae3fbc6340ced0a14d7a90afac145dc
Khoi.LN <147135167+khoilen@users.noreply.github.com>
2025-01-25 09:21:29 +0700

    Feat: add brand colors and Shade primary markdown (#54)
    - Feat: add color brand docs and shade primary

--- apps/nt-headless-ui/.storybook/main.ts
+++ apps/nt-headless-ui/.storybook/main.ts
@@ -1,15 +1,16 @@
-import { dirname, join } from 'path'
-
 module.exports = {
-    stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
+    stories: [
+        '../components/**/*.stories.@(js|jsx|ts|tsx)',
+        '../stories/**/*.@(mdx)',
+    ],
     addons: [
-        getAbsolutePath('@storybook/addon-links'),
-        getAbsolutePath('@storybook/addon-essentials'),
-        getAbsolutePath('@storybook/addon-interactions'),
-        getAbsolutePath('@chromatic-com/storybook'),
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@chromatic-com/storybook',
     ],
     framework: {
-        name: getAbsolutePath('@storybook/nextjs'),
+        name: '@storybook/nextjs',
         options: {},
     },
     staticDirs: ['../public'],
@@ -19,8 +20,4 @@ module.exports = {
     typescript: {
         reactDocgen: 'react-docgen-typescript',
     },
-}
-
-function getAbsolutePath(value: string): string {
-    return dirname(require.resolve(join(value, 'package.json')))
 }
