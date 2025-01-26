a9e8516fc76e7532b4e5b1377f62f30c1ab93712
khoilen <khoilen@nashtechglobal.com>
2025-01-26 20:51:10 +0700

    feat: update config

--- apps/nt-headless-ui/vite.config.ts
+++ apps/nt-headless-ui/vite.config.ts
@@ -1,24 +1,27 @@
+import baseConfig from '@share/vite.base.config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
 
-export default defineConfig({
-    resolve: {
-        alias: {
-            '@headless-ui': path.resolve(__dirname),
-            react: path.resolve('./node_modules/react'),
-            'react-dom': path.resolve('./node_modules/react-dom'),
+export default defineConfig(
+    mergeConfig(baseConfig, {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname),
+                react: path.resolve('./node_modules/react'),
+                'react-dom': path.resolve('./node_modules/react-dom'),
+            },
         },
-    },
-    plugins: [react()],
-    test: {
-        watch: false,
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: path.resolve(__dirname, './setup-test.ts'),
-        coverage: {
-            reporter: ['cobertura', 'lcov'],
-            reportOnFailure: true,
+        plugins: [react()],
+        test: {
+            watch: false,
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: path.resolve(__dirname, './setup-test.ts'),
+            coverage: {
+                reporter: ['cobertura', 'lcov'],
+                reportOnFailure: true,
+            },
         },
-    },
-})
+    }),
+)
