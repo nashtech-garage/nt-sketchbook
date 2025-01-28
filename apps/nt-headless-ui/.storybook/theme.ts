e35a368bd5c2e398b21808293b98568a6c5f4ffb
khoilen <khoilen@nashtechglobal.com>
2025-01-28 11:32:45 +0700

    feat: add logo and theme

---
+++ apps/nt-headless-ui/.storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming'
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Nashtech Storybook',
+    brandUrl: 'https://nashtech-storybook.netlify.app/',
+    brandImage: '/assets/images/nashtech-logo.svg',
+    brandTarget: '_self',
+})
