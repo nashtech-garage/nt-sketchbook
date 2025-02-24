9a43a68d3c069da6499ba198effaab7730446c55
Tin Thai <107792718+tintungtang@users.noreply.github.com>
2025-02-24 08:51:26 +0700

    refactor: restructure (#73)
    Add STRUCTURE.md
    Extract Tailwind integration
    Export SCSS files
    Add Mulish font
    
    Co-authored-by: Tin Thai <tin.thanh.thai@gmail.com>

---
+++ apps/nt-stylesheet/src/integrations/tailwind/themes/spacing.ts
@@ -0,0 +1,26 @@
+export const spacing = {
+    xxxlg: '5rem',
+    xxlg: '4rem',
+    xlg: '3rem',
+    lg: '2.25rem',
+    DEFAULT: '1.5rem',
+    sm: '1.125rem',
+    xs: '0.75rem',
+    xxs: '0.5rem',
+    xxxs: '0.25rem',
+    'page-md': '5rem',
+    'page-sm': '3.75rem',
+    'section-md': '2.5rem',
+    'section-sm': '1.5rem',
+    'section-xs': '1.25rem',
+    'group-md': '1.125rem',
+    'group-sm': '1rem',
+    'group-xs': '0.75rem',
+}
+export const gap = {
+    DEFAULT: '0.75rem',
+    lg: '0.5rem',
+    md: '0.375rem',
+    sm: '0.25rem',
+    xs: '0.125rem',
+}
