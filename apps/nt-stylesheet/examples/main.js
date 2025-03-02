ef0555acb8642a0a49b63a78b286b236a34be0d1
Tin Thai <tin.thanh.thai@gmail.com>
2025-03-02 23:06:38 +0700

    feat: css core
    Refine variables
    Add font and spacing ultilities
    Add base layout style

---
+++ apps/nt-stylesheet/examples/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.getElementById("nt-toggle-menu");
+    const mainPage = document.querySelector(".nt-main-page");
+    const overlay = document.getElementById("nt-overlay");
+
+    //Menu
+    const menuItems = document.querySelectorAll(".nt-main-page-side-menu-item");
+
+    menuItems.forEach((item) => {
+        item.addEventListener("click", function () {
+            // Remove active class from all menu items
+            menuItems.forEach((el) => el.classList.remove("active"));
+
+            // Add active class to the clicked item
+            this.classList.add("active");
+        });
+    });
+});
+
