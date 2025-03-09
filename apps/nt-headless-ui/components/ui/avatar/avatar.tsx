decf55a2ed83d22d35d250833b029424f0b14879
Khoi.LN <147135167+khoilen@users.noreply.github.com>
2025-03-10 06:05:43 +0700

    feat: update avatar (#81)

--- apps/nt-headless-ui/components/ui/avatar/avatar.tsx
+++ apps/nt-headless-ui/components/ui/avatar/avatar.tsx
@@ -3,81 +3,98 @@ import { cn } from '@headless-ui/lib/utils'
 import {
     AvatarFallback,
     AvatarImage,
-    AvatarPrimitive,
     AvatarPrimitiveRoot,
 } from '../../radix/avatar'
 
 export type AvatarSize = 'extra-small' | 'small' | 'medium' | 'large'
+export type BadgePosition =
+    | 'top-left'
+    | 'top-right'
+    | 'bottom-left'
+    | 'bottom-right'
 
 export type AvatarProps = {
     className?: string
     src?: string
     fallBack?: React.ReactNode
     hasBadge?: boolean
-    badgePosition?:
-        | 'top-left'
-        | 'top-right'
-        | 'bottom-left'
-        | 'bottom-right'
+    badgePosition?: BadgePosition
     size?: AvatarSize
     badgeClass?: string
-} & React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+} & React.ComponentPropsWithoutRef<typeof AvatarPrimitiveRoot>
 
-const Avatar = (props: AvatarProps) => {
-    const {
-        src,
-        fallBack,
-        hasBadge = false,
-        badgePosition = 'bottom-right',
-        className = '',
-        size = 'medium',
-        badgeClass = '',
-        ...args
-    } = props
-
-    const sizeClasses = {
-        'extra-small': 'h-7 w-7',
-        small: 'h-10 w-10',
-        medium: 'h-20 w-20',
-        large: 'h-40 w-40',
-    }
-
-    const positionClasses = {
-        'top-left': 'absolute top-[4px] left-[3px]',
-        'top-right': 'absolute top-[4px] right-[3px]',
-        'bottom-left': 'absolute bottom-[3px] left-[1px]',
-        'bottom-right': 'absolute bottom-[10px] right-[18px]',
-    }
-
-    const badgeSize = {
-        'extra-small': 'h-2 w-2',
-        small: 'h-3 w-3',
-        medium: 'h-4 w-4',
-        large: 'h-5 w-5',
-    }
-
-    return (
-        <AvatarPrimitiveRoot
-            className={cn(sizeClasses[size], className)}
-            {...args}
-        >
-            <AvatarImage src={src} />
-            <AvatarFallback>{fallBack || null}</AvatarFallback>
-            {hasBadge && (
-                <span
-                    role="presentation"
-                    className={cn(
-                        'h-4 w-4 rounded-full bg-primary border border-white',
-                        positionClasses[badgePosition!],
-                        badgeSize[size],
-                        badgeClass,
-                    )}
-                >
-                    <span className="px-1 rounded" />
-                </span>
-            )}
-        </AvatarPrimitiveRoot>
-    )
+const sizeClasses: Record<AvatarSize, string> = {
+    'extra-small': 'h-7 w-7',
+    small: 'h-10 w-10',
+    medium: 'h-20 w-20',
+    large: 'h-40 w-40',
 }
+
+const positionClasses: Record<
+    BadgePosition,
+    Record<AvatarSize, string>
+> = {
+    'top-left': {
+        'extra-small': 'top-[5px] left-[-3px]',
+        small: 'top-[5px] left-[-4px]',
+        medium: 'top-[10px] left-0',
+        large: 'top-[10px] left-[18px]',
+    },
+    'top-right': {
+        'extra-small': 'top-[5px] right-[-3px]',
+        small: 'top-[5px] right-[-4px]',
+        medium: 'top-[10px] right-0',
+        large: 'top-[10px] right-[18px]',
+    },
+    'bottom-left': {
+        'extra-small': 'bottom-[5px] left-[-3px]',
+        small: 'bottom-[5px] left-[-4px]',
+        medium: 'bottom-[10px] left-0',
+        large: 'bottom-[10px] left-[18px]',
+    },
+    'bottom-right': {
+        'extra-small': 'bottom-[5px] right-[-3px]',
+        small: 'bottom-[5px] right-[-4px]',
+        medium: 'bottom-[10px] right-0',
+        large: 'bottom-[10px] right-[18px]',
+    },
+}
+
+const badgeSize: Record<AvatarSize, string> = {
+    'extra-small': 'h-2 w-2',
+    small: 'h-3 w-3',
+    medium: 'h-4 w-4',
+    large: 'h-5 w-5',
+}
+
+const Avatar = ({
+    src,
+    fallBack,
+    hasBadge = false,
+    badgePosition = 'bottom-right',
+    className = '',
+    size = 'medium',
+    badgeClass = '',
+    ...args
+}: AvatarProps) => (
+    <AvatarPrimitiveRoot
+        className={cn(sizeClasses[size], className)}
+        {...args}
+    >
+        <AvatarImage src={src} />
+        <AvatarFallback>{fallBack || null}</AvatarFallback>
+        {hasBadge && (
+            <span
+                role="presentation"
+                className={cn(
+                    'absolute rounded-full bg-primary border border-white',
+                    positionClasses[badgePosition][size],
+                    badgeSize[size],
+                    badgeClass,
+                )}
+            />
+        )}
+    </AvatarPrimitiveRoot>
+)
 
 export { Avatar }
