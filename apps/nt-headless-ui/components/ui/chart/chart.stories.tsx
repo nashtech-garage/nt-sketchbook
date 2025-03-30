cc3e9a3875e0127b9c0636b6e2928040889b206a
khoilen <khoilen@nashtechglobal.com>
2025-03-30 16:46:23 +0700

    feat: update storybook

--- apps/nt-headless-ui/components/ui/chart/chart.stories.tsx
+++ apps/nt-headless-ui/components/ui/chart/chart.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryFn } from '@storybook/react'
+import { ChartType } from 'chart.js'
 import React from 'react'
 
 import { Chart, type ChartProps } from './chart'
@@ -9,19 +10,21 @@ export default {
     component: Chart,
 } as Meta
 
-const Template: StoryFn<ChartProps<any>> = (
+const Template: StoryFn<ChartProps<ChartType>> = (
     args: ChartProps<any>,
 ) => <Chart {...args} />
 
-export const Bar: StoryFn<ChartProps<any>> = Template.bind({})
+export const Bar: StoryFn<ChartProps<ChartType>> = Template.bind({})
 Bar.args = {
     type: 'bar',
+    className: 'w-full h-[400px]',
     options: {
         responsive: true,
         plugins: {
             legend: { display: true },
             tooltip: { enabled: true },
         },
+        maintainAspectRatio: true,
     },
     data: {
         labels: ['January', 'February', 'March', 'April', 'May'],
