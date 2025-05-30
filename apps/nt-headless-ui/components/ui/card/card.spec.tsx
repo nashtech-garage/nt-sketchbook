ae883596a8b15dede350ce6f3afe3f5022f360cf
Loc Nguyen Huu <nghloc1603@gmail.com>
2025-05-30 15:58:04 +0700

    feat: update card display, props and stylesheet integration

--- apps/nt-headless-ui/components/ui/card/card.spec.tsx
+++ apps/nt-headless-ui/components/ui/card/card.spec.tsx
@@ -1,3 +1,73 @@
-import { describe } from 'node:test'
+import { render } from '@testing-library/react'
+import React from 'react'
+import { describe, expect, it } from 'vitest'
 
-describe('Card Component', () => {})
+import { Card, type CardProps } from './card'
+
+describe('Card Component', () => {
+    it('renders with title and content', () => {
+        const props: CardProps = {
+            header: 'Card Title',
+            children: 'This is a card content.',
+        }
+        const { getByText } = render(<Card {...props} />)
+        expect(
+            getByText((props.header ?? '').toString()),
+        ).toBeInTheDocument()
+        expect(
+            getByText((props.children ?? '').toString()),
+        ).toBeInTheDocument()
+    })
+
+    it('renders with footer', () => {
+        const props: CardProps = {
+            header: 'Card Title',
+            children: 'This is a card content.',
+            footer: 'This is a card footer.',
+        }
+        const { getByText } = render(<Card {...props} />)
+        expect(
+            getByText((props.header ?? '').toString()),
+        ).toBeInTheDocument()
+        expect(
+            getByText((props.children ?? '').toString()),
+        ).toBeInTheDocument()
+        expect(
+            getByText((props.footer ?? '').toString()),
+        ).toBeInTheDocument()
+    })
+
+    it('renders with icon', () => {
+        const props: CardProps = {
+            header: 'Card Title',
+            children: 'This is a card content.',
+            icon: '📊',
+        }
+        const { getByText, getByRole } = render(<Card {...props} />)
+        expect(
+            getByText((props.header ?? '').toString()),
+        ).toBeInTheDocument()
+        expect(
+            getByText((props.children ?? '').toString()),
+        ).toBeInTheDocument()
+        expect(getByRole('img')).toHaveAttribute(
+            'src',
+            props.icon ?? '',
+        )
+    })
+
+    it('renders with variant', () => {
+        const props: CardProps = {
+            header: 'Card Title',
+            children: 'This is a card content.',
+            variant: 'success',
+        }
+        const { getByText } = render(<Card {...props} />)
+        expect(
+            getByText((props.header ?? '').toString()),
+        ).toBeInTheDocument()
+        expect(
+            getByText((props.children ?? '').toString()),
+        ).toBeInTheDocument()
+    })
+})
