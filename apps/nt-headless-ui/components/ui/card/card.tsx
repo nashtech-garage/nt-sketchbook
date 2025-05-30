ccf51e20c35463440c3cf6844b074aa11c5cc915
Loc Nguyen Huu <nghloc1603@gmail.com>
2025-05-30 15:58:04 +0700

    feat: update card display, props and stylesheet integration

--- apps/nt-headless-ui/components/ui/card/card.tsx
+++ apps/nt-headless-ui/components/ui/card/card.tsx
@@ -3,10 +3,17 @@ import {
     CardDescription,
     CardFooter,
     CardHeader,
+    CardTitle,
     CardWrapper,
 } from '@/components/radix/card'
 import { cn } from '@/lib/utils'
 
+export type CardVariant =
+    | 'small'
+    | 'default'
+    | 'success'
+    | 'danger'
+    | 'primary'
 export type CardProps = {
     children: React.ReactNode
     footer?: React.ReactNode
@@ -16,6 +23,8 @@ export type CardProps = {
     classNameFooter?: string
     classNameContent?: string
     description?: string
+    variant?: CardVariant
+    icon?: string
 } & React.HTMLAttributes<HTMLDivElement>
 const Card: React.FC<CardProps> = ({
     footer,
@@ -25,17 +34,44 @@ const Card: React.FC<CardProps> = ({
     classNameHeader,
     classNameFooter,
     classNameContent,
+    variant = 'default',
     description,
+    icon,
     ...props
 }) => (
-    <CardWrapper className={cn('nt-card', className)} {...props}>
-        <CardContent className={cn(classNameContent)}>
-            <CardHeader className={cn(classNameHeader)}>
-                {header}
+    <CardWrapper
+        className={cn('nt-card', `nt-card-${variant}`, className)}
+        {...props}
+    >
+        <CardContent
+            className={cn('nt-card-content', classNameContent)}
+        >
+            <CardHeader
+                className={cn('nt-card-header', classNameHeader)}
+            >
+                <div className="flex flex-col">
+                    <div className="flex">
+                        {icon && (
+                            <span className={cn('nt-card-icon mr-2')}>
+                                {icon}
+                            </span>
+                        )}
+                        <CardTitle
+                            className={cn(
+                                'nt-card-title',
+                                classNameHeader,
+                            )}
+                        >
+                            {header}
+                        </CardTitle>
+                    </div>
+                </div>
                 <CardDescription>{description}</CardDescription>
             </CardHeader>
             {children}
-            <CardFooter className={cn(classNameFooter)}>
+            <CardFooter
+                className={cn('nt-card-footer', classNameFooter)}
+            >
                 {footer}
             </CardFooter>
         </CardContent>
