bab393137620684112f69f58e26406040e193bc1
khoilen <khoilen@nashtechglobal.com>
2025-06-15 06:55:08 +0700

    feat: update label with stylesheet

--- apps/nt-headless-ui/components/ui/label/label.tsx
+++ apps/nt-headless-ui/components/ui/label/label.tsx
@@ -1,18 +1,32 @@
+import { cn } from '@/lib/utils'
 import { type ReactNode } from 'react'
 
+export type LabelVariant = 'hint' | 'form-field'
+
 export type LabelProps = {
-    htmlFor: string
+    htmlFor?: string
     children: ReactNode
     className?: string
+    variant?: LabelVariant
 }
 
 export const Label = ({
     htmlFor,
     children,
     className,
+    variant = 'form-field'
 }: LabelProps) => {
     return (
-        <label htmlFor={htmlFor} className={className}>
+        <label
+            {...(htmlFor ? { htmlFor } : {})}
+            className={cn(
+                'nt-label',
+                variant === 'hint'
+                    ? 'nt-label-hint'
+                    : 'nt-label-form-field',
+                className
+            )}
+        >
             {children}
         </label>
     )
