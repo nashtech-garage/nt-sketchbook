7b1befe6207d28f925d767175507047139797166
khoilen <khoilen@nashtechglobal.com>
2025-08-02 07:29:02 +0700

    feat: add multiple select

--- apps/nt-stylesheet/src/scripts/index.ts
+++ apps/nt-stylesheet/src/scripts/index.ts
@@ -1,10 +1,12 @@
 import pkg from '../../package.json'
 import { NtModal } from './nt-modal'
+import { NtMultiSelect } from './nt-multi-select'
 import { NtPopover } from './nt-popover'
 import { NtTooltip } from './nt-tooltip'
 
 NtModal.init()
 NtTooltip.init()
 NtPopover.init()
+NtMultiSelect.init()
 
 console.log(`Welcome to NT Stylesheet v${pkg.version} — Ready to go!`)
