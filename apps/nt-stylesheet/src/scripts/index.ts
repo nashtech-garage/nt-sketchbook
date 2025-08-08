c3e201f68050f9e0b81c7ed40bc2081c9f69f2de
khoilen <khoilen@nashtechglobal.com>
2025-08-08 09:33:13 +0700

    feat: add collapse stylesheet

--- apps/nt-stylesheet/src/scripts/index.ts
+++ apps/nt-stylesheet/src/scripts/index.ts
@@ -1,4 +1,5 @@
 import pkg from '../../package.json'
+import { NtCollapse } from './nt-collapse'
 import { NtModal } from './nt-modal'
 import { NtMultiSelect } from './nt-multi-select'
 import { NtPopover } from './nt-popover'
@@ -8,5 +9,6 @@ NtModal.init()
 NtTooltip.init()
 NtPopover.init()
 NtMultiSelect.init()
+NtCollapse.init()
 
 console.log(`Welcome to NT Stylesheet v${pkg.version} — Ready to go!`)
